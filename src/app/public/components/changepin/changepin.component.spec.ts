bb40da7e84d02f8f0bb2d86550c9738d90e734ba
Muskan <44183828+muskandav@users.noreply.github.com>
2022-11-11 12:16:21 +0530

    Update changepin.component.spec.ts

--- src/app/public/components/changepin/changepin.component.spec.ts
+++ src/app/public/components/changepin/changepin.component.spec.ts
@@ -1,23 +1,77 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { ChangepinComponent } from './changepin.component';
 
 describe('ChangepinComponent', () => {
-  let component: ChangepinComponent;
-  let fixture: ComponentFixture<ChangepinComponent>;
+    let component: ChangepinComponent;
+    let fixture: ComponentFixture<ChangepinComponent>;
+    let oldUserPin: number;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ChangepinComponent ]
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ChangepinComponent]
+        })
+            .compileComponents();
+
+        oldUserPin = 1234;
+        fixture = TestBed.createComponent(ChangepinComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should disable submit button if oldPin input is not equal to the user old pin', () => {
+
+        //Arrange
+        component.formData.get('oldPin')?.setValue('2345');
+
+        //Act and Assert
+        expect(fixture.debugElement.query(By.css('button')).nativeElement.disabled).toBeTruthy();
     })
-    .compileComponents();
+    it('should disable submit button if new pin and confirm new pin are not equal', () => {
 
-    fixture = TestBed.createComponent(ChangepinComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        //Arrange
+        component.formData.get('oldPin')?.setValue('2345');
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        //Act and Assert
+        expect(fixture.debugElement.query(By.css('button')).nativeElement.disabled).toBeTruthy();
+    })
+    it('should enable submit button if oldpin is correct and new pin is equal to confirm new pin', () => {
+
+        //Arrange
+        component.formData.get('oldPin')?.setValue('2345');
+        component.formData.get('newPin')?.setValue('1234');
+        component.formData.get('ConfirmNewPin')?.setValue('1234');
+
+        //Act and Assert
+        expect(fixture.debugElement.query(By.css('button')).nativeElement.disabled).toBeFalsy();
+    })
+
+    it('should disable submit button if old pin field is empty', () => {
+        //Arrange
+        component.formData.get('oldPin')?.setValue('');
+
+        //Act and Assert
+        expect(fixture.debugElement.query(By.css('button')).nativeElement.disabled).toBeTruthy();
+    })
+
+    it('should disable submit button if new pin field is empty', () => {
+        //Arrange
+        component.formData.get('newPin')?.setValue('');
+
+        //Act and Assert
+        expect(fixture.debugElement.query(By.css('button')).nativeElement.disabled).toBeTruthy();
+    })
+
+    it('should disable submit button if confirm new pin field is empty', () => {
+        //Arrange
+        component.formData.get('ConfirmNewPin')?.setValue('');
+
+        //Act and Assert
+        expect(fixture.debugElement.query(By.css('button')).nativeElement.disabled).toBeTruthy();
+    })
 });
