59ab5d5e00181e388021a9a8e942c9d9619a1ccc
Muskan <44183828+muskandav@users.noreply.github.com>
2022-11-11 12:52:03 +0530

    Update openfd.component.spec.ts

--- src/app/public/components/openfd/openfd.component.spec.ts
+++ src/app/public/components/openfd/openfd.component.spec.ts
@@ -1,23 +1,66 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { OpenfdComponent } from './openfd.component';
 
 describe('OpenfdComponent', () => {
-  let component: OpenfdComponent;
-  let fixture: ComponentFixture<OpenfdComponent>;
+    let component: OpenfdComponent;
+    let fixture: ComponentFixture<OpenfdComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ OpenfdComponent ]
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [OpenfdComponent]
+        })
+            .compileComponents();
+
+        fixture = TestBed.createComponent(OpenfdComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should disable submit button if from amount field is empty', () => {
+        //Arrange
+        component.formData.get('fromAcc')?.setValue('');
+
+        //Act and Assert
+        expect(fixture.debugElement.query(By.css('button')).nativeElement.disabled).toBeTruthy();
     })
-    .compileComponents();
 
-    fixture = TestBed.createComponent(OpenfdComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    it('should disable submit button if fdProduct field is empty', () => {
+        //Arrange
+        component.formData.get('fdProduct')?.setValue('');
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        //Act and Assert
+        expect(fixture.debugElement.query(By.css('button')).nativeElement.disabled).toBeTruthy();
+    })
+
+    it('should disable submit button if fdPeriod field is empty', () => {
+        //Arrange
+        component.formData.get('fdPeriod')?.setValue('');
+
+        //Act and Assert
+        expect(fixture.debugElement.query(By.css('button')).nativeElement.disabled).toBeTruthy();
+    })
+
+    it('should disable submit button if amount field is empty', () => {
+        //Arrange
+        component.formData.get('amount')?.setValue('');
+
+        //Act and Assert
+        expect(fixture.debugElement.query(By.css('button')).nativeElement.disabled).toBeTruthy();
+    })
+
+    it('should have 2 input fields', () => {
+
+        expect(fixture.debugElement.queryAll(By.css('input')).length).toBe(2);
+    })
+
+    it('should have 2 select fields', () => {
+
+        expect(fixture.debugElement.queryAll(By.css('select')).length).toBe(2);
+    })
 });
